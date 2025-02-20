e327b98cdc9019d33ce698e740087241f4899bfc
amrita-arun <amrita.arun2013@gmail.com>
2025-02-19 21:43:10 -0800

    base app w/ folders

---
+++ healthApp/healthApp/healthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  healthAppApp.swift
+//  healthApp
+//
+//  Created by Administrator on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct healthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
